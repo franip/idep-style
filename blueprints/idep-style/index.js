4fab51dc3c5be0ab34ef597c2f334ee03a588c91
Fran <fipastor@uco.es>
2020-04-19 17:21:31 +0200

    Arreglado fallo al intentar ejecutar el ember generate con el nombre del addon

--- blueprints/idep-style/index.js
+++ blueprints/idep-style/index.js
@@ -23,4 +23,10 @@ module.exports = {
       ]);
     });
   },
+
+  normalizeEntityName() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
 };
